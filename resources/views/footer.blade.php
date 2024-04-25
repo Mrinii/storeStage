c01816dc924b09c83f70ecbc903dff059d451765
Youssef Boughanem <youssefboughanem55gmail.com.>
2024-04-25 22:49:14 +0100

    test update diyal front end

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
-<footer class="footer mt-auto py-3 bg-light">
-  <h1>Footer</h1>
+<footer class="footer mt-auto py-3 bg-transparent fixed-bottom">
   <div class="container">
+    <h1 class="text-white">Footer</h1>
     <div class="row">
       <div class="col-md-6">
-        <h5>Follow Us</h5>
+        <h5 class="text-white">Follow Us</h5>
         <ul class="list-inline">
           <li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>
           <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
@@ -11,11 +11,11 @@
         </ul>
       </div>
       <div class="col-md-6">
-        <h5>Additional Links</h5>
+        <h5 class="text-white">Additional Links</h5>
         <ul class="list-unstyled">
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#" class="text-white">About Us</a></li>
+          <li><a href="#" class="text-white">Contact Us</a></li>
+          <li><a href="#" class="text-white">Privacy Policy</a></li>
         </ul>
       </div>
     </div>
