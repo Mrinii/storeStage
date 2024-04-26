1c3113c659bb14dcdcf7267eb33923823f7a20de
Youssef Boughanem <youssefboughanem55gmail.com.>
2024-04-26 17:58:41 +0100

    Initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,8 @@
     <div class="row">
         @foreach ($produits as $item)
         <div class="col-md-4 mb-4">
-            <div class="card">
-                <img src="{{ asset('images/'.$item['image']) }}" class="card-img-top" alt="Product Image">
+            <div class="card h-100">
+                <img src="{{ asset('images/'.$item['image']) }}" class="card-img-top" alt="Product Image" style="height: 200px; width: 100%; object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title">{{ $item['nom'] }}</h5>
                     <p class="card-text">{{ $item['prix'] }} dh</p>
