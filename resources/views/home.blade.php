eca700f05d4676bea60a585d915f68d57a22ba93
Youssef Boughanem <youssefboughanem55gmail.com.>
2024-04-26 17:57:01 +0100

    add card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,24 @@
 @extends('master_page')
 @section('title','Acceuil')
 @section('content')
-<h1 class="text-center">Bienvenue sur notre site e-commerce</h1>
-<p class="text-center">Découvrez nos produits de qualité à des prix compétitifs</p>
-    @foreach ($produits as $item)
-    <ul>
-        <li>{{$item['nom']}}</li>
-        <li>{{$item['prix']}} dh</li>
-        <li>{{$item['categorie']}}</li>
-        <li><img src="{{asset('images/'.$item['image'])}}" alt="img" width="100"></li>
-        <li>
-<a href="{{ url('cart/addc', ['id' => $item['id']]) }}" class="btn btn-block text-center text-dark" role="button">Ajouter au cart</a>
-<a href="/details/{{$item['id']}}">Details</a>
-        </li>
-    </ul>
-@endforeach
+<div class="container">
+    <h1 class="text-center">Bienvenue sur notre site e-commerce</h1>
+    <p class="text-center">Découvrez nos produits de qualité à des prix compétitifs</p>
+    <div class="row">
+        @foreach ($produits as $item)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="{{ asset('images/'.$item['image']) }}" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $item['nom'] }}</h5>
+                    <p class="card-text">{{ $item['prix'] }} dh</p>
+                    <p class="card-text">{{ $item['categorie'] }}</p>
+                    <a href="{{ url('cart/addc', ['id' => $item['id']]) }}" class="btn btn-block btn-dark">Ajouter au panier</a>
+                    <a href="/details/{{ $item['id'] }}" class="btn btn-link">Détails</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
-
-
-
-<!-- Inclure jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-
-<!-- Inclure les fichiers JavaScript de Bootstrap 4 -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min.js"></script>
--- resources/views/master_page.blade.php
+++ resources/views/master_page.blade.php
@@ -8,13 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>@yield('title')</title>
     <style>
-body{
+/* body{
 
     background-image: url('/images/jordan-shoes-4k-3k7ucbqt0por3w1j.jpg');
    background-repeat: no-repeat;
    background-size: 100%;
 
-}
+} */
 .navbar {
   background-color: transparent !important;
 }
